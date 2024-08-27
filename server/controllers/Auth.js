46690424f93998e56ada6ec77a48c38eba024fd2
shraddha1dir@gmail.com <shraddha1dir@gmail.com>
2024-08-27 20:43:14 +0530

    nodemailer updates

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -203,9 +203,7 @@ exports.sendotp = async (req, res) => {
       specialChars: false,
     })
     const result = await OTP.findOne({ otp: otp })
-    console.log("Result is Generate OTP Func")
-    console.log("OTP", otp)
-    console.log("Result", result)
+
     while (result) {
       otp = otpGenerator.generate(6, {
         upperCaseAlphabets: false,
@@ -213,7 +211,7 @@ exports.sendotp = async (req, res) => {
     }
     const otpPayload = { email, otp }
     const otpBody = await OTP.create(otpPayload)
-    console.log("OTP Body", otpBody)
+
     res.status(200).json({
       success: true,
       message: `OTP Sent Successfully`,
